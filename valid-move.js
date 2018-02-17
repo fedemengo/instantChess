8e89aee51454e8b389535ab9747d7ebf5320e42a
fedemengo <fede.mengo@hotmail.it>
2018-02-17 11:10:02 +0100

    code clean up

--- valid-move.js
+++ valid-move.js
@@ -4,53 +4,33 @@ function compareTo(a, b){
 
 var validMove = {
     pawn: function (board, pieceID, from, to) {
-        var dist = 1;
-        if(!board.firstMove.has(pieceID)){
-            dist = 2;
-        }
+        var dist = board.firstMove.has(pieceID) ? 1 : 2;
+        var movementDist = (pieceID[0] == 'w' ? to.row - from.row : from.row - to.row);
+
+        // move forward - don't take OR move diagonal - attempt take
         if(from.col == to.col){
-            if(pieceID[0] == 'w'){
-                if(to.row - from.row > dist || to.row - from.row < 0 || board[to.col + to.row]){
-                    return false;
-                }
-            } else {
-                if(from.row - to.row > dist || from.row - to.row < 0 || board[to.col + to.row]){
-                    return false;
-                }
-            }
-        } else if(from.col != to.col) {
-            if (pieceID[0] == 'w') {
-                if (to.row - from.row != 1) {
-                    return false;
-                }
-            } else if(pieceID[0] == 'b'){
-                if (from.row - to.row != 1) {
-                    return false;
-                }
-            }
-            if((!board[to.col + to.row]) || (board[to.col + to.row] && board[to.col + to.row][0] == pieceID[0])) {
-                return false;
-            }
+            // position is empty AND moved forward of right amount - IS NOT VALID
+            return !board[to.col + to.row] && movementDist > 0 && movementDist < dist;
+        } else {   /*|| board[to.col + to.row][0] == pieceID[0]*/
+            // row diagonal movement is exactly 1 AND position is not empty, the piece is not of same color (already checked for every piece)
+            return movementDist == 1 && board[to.col + to.row];
         }
-        return true;
     },
     rook: function (board, pieceID, from, to) {
-        if(from.col != to.col && from.row != to.row) {
-            return false;
-        }
-        return validMove.longMove(board, from, to);
+        // the move is potentially valid, check if there piece in the middle of the movement
+        return (from.col == to.col || from.row == to.row) && validMove.longMove(board, from, to);
     },
     knight: function (from, to) {
-        var moveX = Math.abs(from.row-to.row);
+        var moveX = Math.abs(parseInt(from.row)-parseInt(to.row));
         var moveY = Math.abs(from.col.charCodeAt(0)-to.col.charCodeAt(0));
-        return ((moveX == 2 && moveY == 1) || (moveX == 1 && moveY == 2));
+        // move should be a L
+        return (moveX == 2 && moveY == 1) || (moveX == 1 && moveY == 2);
     },
     bishop: function (board, pieceID, from, to) {
-        var fromCol = from.col.charCodeAt(0), toCol = to.col.charCodeAt(0);
-        if(from.col == to.col || from.row == to.row || Math.abs(fromCol-toCol) != Math.abs(from.row-to.row)) {
-            return false;
-        }
-        return validMove.longMove(board, from, to);
+        var moveX = Math.abs(parseInt(from.row)-parseInt(to.row));
+        var moveY = Math.abs(from.col.charCodeAt(0) - to.col.charCodeAt(0));
+        // the move is potentially valid, check if there piece in the middle of the movement
+        return moveX == moveY && validMove.longMove(board, from, to);
     },
     queen: function (board, pieceID, from, to) {
         return validMove.rook(board, pieceID, from, to) || validMove.bishop(board, pieceID, from, to);
@@ -58,10 +38,10 @@ var validMove = {
     longMove: function (board, from, to){
         var incY = compareTo(from.col, to.col), incX = compareTo(from.row, to.row);
         var minY = from.col.charCodeAt(0), maxY = to.col.charCodeAt(0);
-        var minX = from.row, maxX = to.row;
+        var minX = parseInt(from.row), maxX = parseInt(to.row);
 
-        for(var i=minY+incY, j=parseInt(minX)+incX; i!=maxY || j!=maxX; i+=incY, j+=incX){
-            //console.log(String.fromCharCode(i) + j);
+        for(var i=minY+incY, j=minX+incX; i!=maxY || j!=maxX; i+=incY, j+=incX){
+            console.log(String.fromCharCode(i) + j);
             if(board[String.fromCharCode(i) + j]) {
                 return false;
             }
