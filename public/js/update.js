c893bbb031b870b5854512b8411c1e5b50716985
fedemengo <fede.mengo@hotmail.it>
2018-04-13 23:09:56 +0200

    fix bug when multiple takes occour on the same positin and refactor

--- public/js/update.js
+++ public/js/update.js
@@ -1,31 +1,34 @@
-var color, socket, colors = {"w": "white", "b": "black"}, nextMove = {"w": "black", "b": "white"};
-$(function () {
+var color, socket, nextMove = {"w": "black", "b": "white"}, SQUARE_SIZE = 45;;
+$(() => {
     // banner displaying opponent's url
     if(!parseInt(window.location.href.slice(-1)))
-		$("#url")[0].text = window.location.href.slice(0, -1) + "1";
+		$("#url").first().text(window.location.href.slice(0, -1) + "1");
 	else
-		$(".alert.alert-info")[0].remove();
+        $(".alert.alert-info").first().remove();
+
     socket = io();
-    socket.on("notify", function (data) {
+    socket.on("notify", (data) => {
 
 		var result = data.moveResult;
-        var elem = document.querySelectorAll("div[col='" + result.from[0] + "'][row='" + result.from[1] + "'][color='" + result.color + "']")[0];
-        color = String($("#player-color").text()).split(" ")[0][0];
-
+        color = $("#player-color").text().split(" ")[0][0];
+        
 		// update player turn
         $("#move").text(nextMove[result.color] + " move");
-
+        
 		// if the move is from the other player then update  board
         if (result.color !== color){
+            var elem = $("div[col='" + result.from[0] + "'][row='" + result.from[1] + "'][color='" + result.color + "']").get(0);
+            var left = parseInt($(elem).css("left").slice(0, -2)) + data.x * SQUARE_SIZE;
+            var top = parseInt($(elem).css("top").slice(0, -2)) + data.y * SQUARE_SIZE;
 
-            elem.style.left = String(parseInt(elem.style.left.slice(0, -2)) + parseInt(String(data.x*45)));
-            elem.style.top = String(parseInt(elem.style.top.slice(0, -2)) + parseInt(String(data.y*45)));
-
-			elem.setAttribute("row", result.to[1]);
-			elem.setAttribute("col", result.to[0]);
+            $(elem).attr({
+                "style": "left: " + left + "px; top: " + top + "px;",
+                "row": result.to[1],
+                "col": result.to[0]
+            });
 
 			// re-enable the possibility to move
-            Array.from($(".disabled")).forEach(function (entry) {
+            Array.from($(".disabled")).forEach(entry => {
             	entry.classList.remove("disabled");
             	entry.classList.add("draggable");
 			});
@@ -34,14 +37,22 @@ $(function () {
         // if take hide the taken piece
         if(result.flags === "c"){
             var color = (result.color === "w" ? "b" : "w");
-			document.querySelectorAll("div[col='" + result.to[0] + "'][row='" + result.to[1] + "'][color='" + color + "']")[0].style.visibility = "hidden";
+            $("div[col='" + result.to[0] + "'][row='" + result.to[1] + "'][color='" + color + "']").first().attr({
+                "style": "visibility: hidden;",
+                "row": "-1",
+                "col": "-1" 
+            });
         } else if(result.flags === "e"){
 			var color = (result.color === "w" ? "b" : "w");
-			document.querySelectorAll("div[col='" + result.to[0] + "'][row='" + result.from[1] + "'][color='" + color + "']")[0].style.visibility = "hidden";
-		}
+            $("div[col='" + result.to[0] + "'][row='" + result.from[1] + "'][color='" + color + "']").first().attr({
+                "style": "visibility: hidden;",
+                "row": "-1",
+                "col": "-1" 
+            });    
+        }
 
         // update move listing
-        if(colors[result.color] === "white"){
+        if(result.color === "w"){
             $("<li></li>", { text: result.san }).appendTo("#list");
         } else {
             $("#list").find("li")[$("#list").find("li").length-1].textContent += ", " + result.san;
@@ -49,6 +60,6 @@ $(function () {
     });
 });
 
-function sendWithSocket(msg, data) {
+sendWithSocket = (msg, data) => {
     socket.emit(msg, data);
 }
