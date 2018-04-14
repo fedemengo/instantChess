2453b6bc58655de6b5d3301cbba9082d553b8603
fedemengo <fede.mengo@hotmail.it>
2018-04-14 10:56:15 +0200

    refactor

--- public/js/drag.js
+++ public/js/drag.js
@@ -1,4 +1,4 @@
-var flag = 0, oldCoords, oldPos, newPos, SQUARE_SIZE = 45;
+var flag = 0, oldCoords, oldPos, newCoords, newPos, SQUARE_SIZE = 45;
 // target elements with the "draggable" class
 interact(".draggable")
 
@@ -9,104 +9,107 @@ interact(".draggable")
     restrict: { restriction: "parent", endOnly: true, elementRect: { top: 0, left: 0, bottom: 1, right: 1 } },
     // enable autoScroll
     autoScroll: true,
-
     // call this function on every dragmove event
     onmove: event => {
-        // keep the dragged position in the x/y attributes
         var target = event.target;
-
 		// store the original position
 		if(!flag){
-			flag = 1;
-			target.style.zIndex = "6";
-			oldCoords = {
-				x: target.style.left.slice(0, -2),
-				y: target.style.top.slice(0, -2)
-			};
-			oldPos = {
-				col: target.attributes.col.value,
-				row: target.attributes.row.value
-			};
-			console.log("Init");
-			console.log(oldCoords);
+            flag = 1;
+            $(target).css({"zIndex": "6"});
+            
+			oldCoords = { 
+                "x": $(target).css("left").slice(0, -2), 
+                "y": $(target).css("top").slice(0, -2)
+            };
+
+			oldPos = { 
+                "col": $(target).attr("col"), 
+                "row": $(target).attr("row")
+            };
+			//console.log(oldCoords);
 			console.log(oldPos);
 		}
-
-        var x = (parseFloat(target.getAttribute("x")) || 0) + event.dx,
-            y = (parseFloat(target.getAttribute("y")) || 0) + event.dy;
+        var x = (parseFloat($(target).attr("x")) || 0) + event.dx,
+            y = (parseFloat($(target).attr("y")) || 0) + event.dy;
 
         // update attribute
-        target.style.webkitTransform = target.style.transform = "translate(" + x + "px, " + y + "px)";
-        target.setAttribute("x", x);
-        target.setAttribute("y", y);
-    },
+        $(target).css({
+            "webkitTransform": "translate(" + x + "px, " + y + "px)",
+            "transform": "translate(" + x + "px, " + y + "px)"
+        });
 
+        $(target).attr({ 
+            "x": x, 
+            "y": y 
+        });
+    },
     // call this function on every drag-end event
     onend: event => {
         flag = 0;
         var target = event.target;
+        var x = Math.round((parseFloat($(target).attr("x"))) / SQUARE_SIZE) * SQUARE_SIZE,
+            y = Math.round((parseFloat($(target).attr("y"))) / SQUARE_SIZE) * SQUARE_SIZE;
 
-        var x = Math.round((parseFloat(target.getAttribute("x")))/SQUARE_SIZE)*SQUARE_SIZE,
-            y = Math.round((parseFloat(target.getAttribute("y")))/SQUARE_SIZE)*SQUARE_SIZE;
+        newCoords = { 
+            "x": x, 
+            "y": y 
+        };
 
-        //console.log(x + " " + y);
-		newCoords = {
-			x: x,
-			y: y
-		};
+        var dx = x, dy = y; 
+        $(target).attr("color") === "b" ? dx = -dx : dy = -dy;
+        newPos = {
+            "col": String.fromCharCode(oldPos.col.charCodeAt(0) + parseInt(dx / SQUARE_SIZE)),
+            "row": (parseInt(oldPos.row) + (dy / SQUARE_SIZE)).toString()
+        };
 
-        newPos = coord2Pos(target, x/SQUARE_SIZE, y/SQUARE_SIZE, oldPos);
-
-        //console.log("[" + oldPos.row + ", " + oldPos.col + "] - [" + row + ", " + col + "]");
-        //console.log(row + " " + col);
-
-		console.log("Final");
+		//console.log(newCoords);
 		console.log(newPos);
-		console.log(newCoords);
 
         $.post("/validate", {
             oldPos: oldPos,
 			newPos: newPos,
 			gameID: window.location.search.substr(1).slice(0, -1)
-        }, (valid) => {
-            if(valid.valid){
-
-                target.style.webkitTransform = target.style.transform = "translate(0px, 0px)";
-                target.style.left = parseInt(target.style.left.slice(0, -2)) + parseInt(x) + "px";
-				target.style.top = parseInt(target.style.top.slice(0, -2)) + parseInt(y) + "px";
-
-				target.setAttribute("row", newPos.row);
-                target.setAttribute("col", newPos.col);
-				target.setAttribute("x", "0px");
-				target.setAttribute("y", "0px");
+        }, res => {
+            if(res.valid){
+                $(target).css({
+                    "webkitTransform": "translate(0px, 0px)",
+                    "transform": "translate(0px, 0px)",
+                    "left": parseInt($(target).css("left").slice(0, -2)) + parseInt(x) + "px",
+                    "top": parseInt($(target).css("top").slice(0, -2)) + parseInt(y) + "px"
+                });
+                
+                $(target).attr({
+                    "row": newPos.row,
+                    "col": newPos.col,
+                    "x": "0px",
+                    "y": "0px"
+                });
 
                 sendWithSocket("move", {
-                	pieceID: target.id,
-                    x: -Math.round(x/SQUARE_SIZE),
-                    y: -Math.round(y/SQUARE_SIZE),
-                    row: [oldPos.row, newPos.row],
-                    col: [oldPos.col, newPos.col],
-					moveResult: valid.data
+                	"pieceID": target.id,
+                    "x": - Math.round(x / SQUARE_SIZE),
+                    "y": - Math.round(y / SQUARE_SIZE),
+                    "row": [oldPos.row, newPos.row],
+                    "col": [oldPos.col, newPos.col],
+					"moveResult": res.data
                 });
 
                 Array.from($(".draggable")).forEach((entry) => {
-                	entry.classList.add("disabled");
-					entry.classList.remove("draggable");
+                	$(entry).addClass("disabled");
+					$(entry).removeClass("draggable");
                 });
             } else {
-				target.style.webkitTransform = target.style.transform = "translate(0px, 0px)";
-                target.setAttribute("x", "0px");
-                target.setAttribute("y", "0px");
+                $(target).css({
+                    "webkitTransform": "translate(0px, 0px)",
+                    "transform": "translate(0px, 0px)"
+                });
+                
+                $(target).attr({ 
+                    "x": "0px",
+                    "y": "0px" 
+                });
             }
         });
-        target.style.zIndex = "5";
+        $(target).css({"zIndex": "5"});
     }
 });
-
-coord2Pos = (target, x, y, oldPos) => {
-	target.attributes.color.value === "b" ? x = -x : y = -y;
-	return {
-		col: String.fromCharCode(oldPos.col.charCodeAt(0) + parseInt(x)),
-		row: (parseInt(oldPos.row) + y).toString()
-	}
-}
--- public/js/update.js
+++ public/js/update.js
@@ -1,61 +1,52 @@
 var color, socket, nextMove = {"w": "black", "b": "white"}, SQUARE_SIZE = 45;;
 $(() => {
     // banner displaying opponent's url
-    if(!parseInt(window.location.href.slice(-1)))
+    if (!parseInt(window.location.href.slice(-1))) {
 		$("#url").first().text(window.location.href.slice(0, -1) + "1");
-	else
+    } else {
         $(".alert.alert-info").first().remove();
+    }
 
     socket = io();
-    socket.on("notify", (data) => {
-
-		var result = data.moveResult;
+    socket.on("notify", res => {
+		var move = res.moveResult;
         color = $("#player-color").text().split(" ")[0][0];
         
 		// update player turn
-        $("#move").text(nextMove[result.color] + " move");
+        $("#move").text(nextMove[move.color] + " move");
         
 		// if the move is from the other player then update  board
-        if (result.color !== color){
-            var elem = $("div[col='" + result.from[0] + "'][row='" + result.from[1] + "'][color='" + result.color + "']").get(0);
-            var left = parseInt($(elem).css("left").slice(0, -2)) + data.x * SQUARE_SIZE;
-            var top = parseInt($(elem).css("top").slice(0, -2)) + data.y * SQUARE_SIZE;
+        if (move.color !== color){
+            var elem = $("div[col='" + move.from[0] + "'][row='" + move.from[1] + "'][color='" + move.color + "']").get(0);
+            var left = parseInt($(elem).css("left").slice(0, -2)) + res.x * SQUARE_SIZE;
+            var top = parseInt($(elem).css("top").slice(0, -2)) + res.y * SQUARE_SIZE;
 
             $(elem).attr({
                 "style": "left: " + left + "px; top: " + top + "px;",
-                "row": result.to[1],
-                "col": result.to[0]
+                "row": move.to[1],
+                "col": move.to[0]
             });
 
 			// re-enable the possibility to move
             Array.from($(".disabled")).forEach(entry => {
-            	entry.classList.remove("disabled");
-            	entry.classList.add("draggable");
+            	$(entry).removeClass("disabled");
+            	$(entry).addClass("draggable");
 			});
         }
 
         // if take hide the taken piece
-        if(result.flags === "c"){
-            var color = (result.color === "w" ? "b" : "w");
-            $("div[col='" + result.to[0] + "'][row='" + result.to[1] + "'][color='" + color + "']").first().attr({
-                "style": "visibility: hidden;",
-                "row": "-1",
-                "col": "-1" 
-            });
-        } else if(result.flags === "e"){
-			var color = (result.color === "w" ? "b" : "w");
-            $("div[col='" + result.to[0] + "'][row='" + result.from[1] + "'][color='" + color + "']").first().attr({
-                "style": "visibility: hidden;",
-                "row": "-1",
-                "col": "-1" 
-            });    
+        var color = (move.color === "w" ? "b" : "w");
+        if(move.flags === "c"){
+            $("div[col='" + move.to[0] + "'][row='" + move.to[1] + "'][color='" + color + "']").first().remove();
+        } else if(move.flags === "e"){
+			$("div[col='" + move.to[0] + "'][row='" + move.from[1] + "'][color='" + color + "']").first().remove();
         }
 
         // update move listing
-        if(result.color === "w"){
-            $("<li></li>", { text: result.san }).appendTo("#list");
+        if(move.color === "w"){
+            $("<li></li>", { text: move.san }).appendTo("#list");
         } else {
-            $("#list").find("li")[$("#list").find("li").length-1].textContent += ", " + result.san;
+            $("#list").find("li").last().append(", " + move.san);
         }
     });
 });
